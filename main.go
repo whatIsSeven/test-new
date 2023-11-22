1e85e993302526f103832ad45739cb71cfa4654b
zhenchao <zhenchao@vchangyi.com>
2023-11-22 17:16:10 +0800

    buildx

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
-//$ sudo 
+//$ sudo docker login --username=18392643987@163.com registry.cn-beijing.aliyuncs.com
 
 func main() {
 	http.HandleFunc("/", HelloHandler)
